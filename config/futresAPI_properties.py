df196cef6fdbcc926e999d6cc903af23db3132b2
prasiddhigyawali <prasiddhigyawali@gmail.com>
2020-11-11 16:47:29 -0700

    properties

--- config/futresAPI_properties.py
+++ config/futresAPI_properties.py
@@ -1,16 +1,13 @@
-import configparser
-import parser
-from configparser import SafeConfigParser
+from ConfigParser import SafeConfigParser
 
 parser = SafeConfigParser()
 parser.read('db.ini')
+
 
 host = parser.get('geomedb', 'url')
 user = parser.get('geomedb', 'Username')
 passwd = parser.get('geomedb', 'Password')
 
-
-
-print(f'Host: {host}')
-print(f'User: {user}')
-print(f'Password: {passwd}')
+print(host)
+print(user)
+print(passwd)
